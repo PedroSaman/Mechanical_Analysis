66985b408e6c14901d7bd4b2f741a5f67ac6c27d
Pedro Saman Lenovo <pedrosaman96@gmail.com>
2021-12-23 17:48:58 -0300

    Developing the new adjoin function
    first part is correct

--- M Files/adjoin_2.m
+++ M Files/adjoin_2.m
@@ -1,4 +1,4 @@
-function A = adjoin(line)
+function A = adjoin_2(line)
 %Find the pairs of adjacent blocks in this layer from the block model. Because it finds the pair of block, in the Y axis it only checks to the top 
 %and in the X axis only checks to the right.
 %input: line: each knob in a given layer. (x,y,z,block_type,color,block_number,knob_index)
@@ -17,7 +17,18 @@ for n = 1 : blocks(1) %for every knob in this layer
     if(same_block ~= 0) %If the knob is from the same block, continue. This will be computed until it reaches the next block.
         same_block = same_block - 1; %Generic subtractor
     else
-    col = floor(line(n, 4)/10);                      %number of collums
-    row = 10*((line(n, 4)/10)-floor(line(n, 4)/10)); %number of rows
+        col = floor(line(n, 4)/10);                      %number of collums
+        row = round(10*((line(n, 4)/10)-floor(line(n, 4)/10))); %number of rows
+        x = -ones(row,5); y = -ones(col,5);
+        check = row*col;
+        same_block = check-1;
 
+        for i=0:row-1
+            x(i+1,1) = line(n,1) + col; x(i+1,2) = line(n,2) + i;
+        end
+        for i=0:col-1
+            y(i+1,1) = line(n,1) + i;   y(i+1,2) = line(n,2) + row;
+        end
+    end
+    A = 1;
 end
