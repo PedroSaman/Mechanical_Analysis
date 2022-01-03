a513affa2168932694f5221d394f104a7b994b6c
Pedro Saman Lenovo <pedrosaman96@gmail.com>
2022-01-03 11:34:57 -0300

    Update to force_position.m
    Adjusted the fnx and fny cases to better match the already organized fnz case

--- M Files/force_position.m
+++ M Files/force_position.m
@@ -10,51 +10,49 @@ function FP = force_position(col,row,force_type)
 %        the correct usage, it is necessary a carrefull analysis to
 %        separete which force is which in the function that call this one
 
-if(force_type == "ff" || force_type == "fnx" || force_type == "fny" || force_type == "fnz")
-    %filler
-end
-
-if(force_type == "fnx") %Fnx
-    
-    k=1; %auxiliary variable (optimization)
-    m=1; %auxiliary variable (optimization)
+if(force_type == "fnx")
+    force_index = 1; %force index in FP matrix
     sum_row=row*(1+row)/2; %number of terms (optimization)
-    FPx = zeros(sum_row,3); %preallocation
+    FP = zeros(sum_row,3); %preallocation (optimization)
     
-    for i = (-row/2):(row/2 - 1)
-        for j = (-row/2 + k):row/2
-           f1 = [4*col/2, 4*i];
-           f2 = [4*col/2, 4*j];
-           FPx(m:m+3,:) = [[f1,-1.5];[f2,-1.5];[f1,1.5];[f2,1.5]];
-           m = m + 4;
+    for i=0:row %first of the force pair
+        for j=1:row %second of the force pair
+            if(j <= i) %if the second force is behind the first one
+                continue;
+            end
+            %force position, already with the origin correction
+            f1 = 4*[col/2,i-row/2];
+            f2 = 4*[col/2,j-row/2];
+            
+            %Adding z axis value to f
+            FP(force_index:force_index+3,:) = [[f1,-1.5];[f2,-1.5];[f1,1.5];[f2,1.5]];
+            force_index = force_index + 4;
         end
-        k = k + 1;
-    end
-    FP = [FPx;[-FPx(:,1),FPx(:,2),FPx(:,3)]];
-    
-elseif(force_type == "fny") %Fny
-    
-    k=1; %auxiliary variable (optimization)
-    m=1; %auxiliary variable (optimization)
+    end  
+elseif(force_type == "fny")
+    force_index = 1; %force index in FP matrix
     sum_col=col*(1+col)/2; %number of terms (optimization)
-    FPy = zeros(sum_col,3); %preallocation (optimization)
+    FP = zeros(sum_col,3); %preallocation (optimization)
     
-    for i = (-col/2):(col/2 - 1)
-        for j = (-col/2 + k):col/2
-           f1 = [4*i, 4*row/2];
-           f2 = [4*j, 4*row/2];
-           FPy(m:m+3,:) = [[f1,-1.5];[f2,-1.5];[f1,1.5];[f2,1.5]];
-           m = m + 4;
+    for i=0:col %first of the force pair
+        for j=1:col %second of the force pair
+            if(j <= i) %if the second force is behind the first one
+                continue;
+            end
+            %force position, already with the origin correction
+            f1 = 4*[i-col/2,row/2];
+            f2 = 4*[j-col/2,row/2];
+            
+            %Adding z axis value to f
+            FP(force_index:force_index+3,:) = [[f1,-1.5];[f2,-1.5];[f1,1.5];[f2,1.5]];
+            force_index = force_index + 4;
         end
-        k = k + 1;
     end
-    FP = [FPy;[FPy(:,1),-FPy(:,2),FPy(:,3)]];
-
-elseif(force_type == "fnz") %funciona, vou fazer igual para a logica do fnx e fny
-    force_index = 1;
-    sum_row=row*(1+row)/2;
-    sum_col=col*(1+col)/2;
-    FPz = zeros(sum_row*sum_col,3);
+elseif(force_type == "fnz")
+    force_index = 1; %force index in FP matrix
+    sum_row=row*(1+row)/2; %number of terms in Y axis (optimization)
+    sum_col=col*(1+col)/2; %number of terms in X axis (optimization)
+    FP = zeros(sum_row*sum_col,3); %preallocation (optimization)
 
     for i=1:col %increases the block size in the X axis
         for j=1:row %increases the block size in the Y axis
@@ -78,13 +76,15 @@ elseif(force_type == "fnz") %funciona, vou fazer igual para a logica do fnx e fn
                     f3 = 4*[f3(1)-col/2,f3(2)-row/2];
                     f4 = 4*[f4(1)-col/2,f4(2)-row/2];
 
-                    %Adding to f and z value
-                    FPz(force_index:force_index+3,:) = [[f1,1.5];[f2,1.5];[f3,1.5];[f4,1.5]];
+                    %Adding z axis value to f
+                    FP(force_index:force_index+3,:) = [[f1,1.5];[f2,1.5];[f3,1.5];[f4,1.5]];
                     force_index = force_index + 4;
                 end
             end
         end
     end
-    FP = [FPz;[FPz(:,1),FPz(:,2),-FPz(:,3)]];
-    
+elseif(force_type == "ff")
+    %Need to think about this case
+else
+    FP = [];
 end
