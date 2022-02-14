4953a6f1f0954eb5c38027c2e8318fe0cbdb9278
Pedro Saman Lenovo <pedrosaman96@gmail.com>
2022-02-14 15:10:36 -0300

    Created BigScript.m
    Just a script to call Several case tests at once.

---
+++ M Files/BigScript.m
@@ -0,0 +1,133 @@
+Result = [];
+
+filename = '../Dat Files/A_1.dat'; %#ok<*NASGU>
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/A_2.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/B_1.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/D_1.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/D_2.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/D_3.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/D_4.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/D_5.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/E_1.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/E_2.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/E_3.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_21r3.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_21r4.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_22d3.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_22d4.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_22d5.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_22r3.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_22r4.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_22r5.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_22u3.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_22u4.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_22u5.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/fail_30.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/sample.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/sample1.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/sample2.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/sample3.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/sample4.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/sample5.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/sample6.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/test1.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/test2.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
+
+filename = '../Dat Files/tower.dat';
+Stability_Judge_Opt
+Result = [Result;round(CM,3)];
