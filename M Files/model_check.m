d0b585d1b40835dd047e023d514f29baf7cc5488
Pedro Saman ThinkPad <pedrosaman96@gmail.com>
2022-05-23 13:58:47 +0900

    Update model_check.m
    Added the density map.

--- M Files/model_check.m
+++ M Files/model_check.m
@@ -1,10 +1,14 @@
 function check = model_check(model,M)
+
+base_length = 300;
+base_width = 300;
 z_max = lines(model);
 model_size = size(model,1);
 j = 1;
 check_size = 0;
+map = zeros(base_length,base_width,z_max);
+density_map = zeros(base_length,base_width);
 for i = 1 : z_max
-    map = zeros(20,20);
     while(model(j,4) == i)
         for m = 1 : size(M,1)
             if(model(j,5) == M(m,1))
@@ -13,17 +17,18 @@ for i = 1 : z_max
             end
         end
         if(check_size == 0) %This block type is not available in the lab
-            check = -1;
-            return;
+            %check = -1;
+            %return;
         end
         [col,row] = col_row_converter(model(j,5));
         for k = model(j,2) : model(j,2)+col-1 %Column iterator
             for l = model(j,3) : model(j,3)+row-1 %Row iterator
-                if(map(l,k) == 1)
+                if(map(base_width-l+1,k,i) == 1)
                     check = -2;
                     return;
                 end
-                map(l,k) = 1;
+                map(base_width-l+1,k,i) = 1;
+                density_map(base_width-l+1,k) = density_map(base_width-l+1,k) + 1;
             end
         end
         j = j + 1;
