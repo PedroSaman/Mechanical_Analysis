cbd1db2be97f695ad03b5c07db102af575c503f5
Pedro Saman ThinkPad <pedrosaman96@gmail.com>
2022-06-14 18:13:55 +0900

    Update putcolor.m
    Added all current available blocks and changed how the count for block number are done. Also updated the commentaries

--- M Files/putcolor.m
+++ M Files/putcolor.m
@@ -1,22 +1,38 @@
 function y = putcolor(model)
-%Put up colors to the model loaded, this function is manual used to make
-%the blocks in the future simulations more recognizable. Also add to the
-%model matrix a column with the block number.
+%Put up colors and enumerate the model blocks.
 %input: model:(x, y, z, block_type)
 %output: y:(BlockNo., x, y, z, type, color)
 
-y = model;
-k = size(y);
-number = 1:1:k(1); 
-for n = 1 : k(1)
-    if(y(n, 4) == 22)
-        y(n, 5) = 1;
-    elseif(y(n, 4) == 12)
-        y(n, 5) = 1;
-    elseif(y(n, 4) == 21)
-        y(n, 5) = 1;
+k = size(model,1);
+y = zeros(k,6);
+y(1:k,2:5) = model(1:k,1:4);
+for n = 1 : k
+    if(model(n, 4) == 11)
+        y(n, 6) = 1;
+    elseif(model(n, 4) == 12)
+        y(n, 6) = 1;
+    elseif(model(n, 4) == 21)
+        y(n, 6) = 1;
+    elseif(model(n, 4) == 22)
+        y(n, 6) = 1;
+    elseif(model(n, 4) == 13)
+        y(n, 6) = 1;
+    elseif(model(n, 4) == 31)
+        y(n, 6) = 1;
+    elseif(model(n, 4) == 14)
+        y(n, 6) = 1;
+    elseif(model(n, 4) == 41)
+        y(n, 6) = 1;
+    elseif(model(n, 4) == 24)
+        y(n, 6) = 1;
+    elseif(model(n, 4) == 42)
+        y(n, 6) = 1;
+    elseif(model(n, 4) == 28)
+        y(n, 6) = 1;
+    elseif(model(n, 4) == 82)
+        y(n, 6) = 1;
     else
-        y(n, 5) = 1;
+        y(n, 6) = 1;
     end
+    y(n,1) = n;
 end
-y = [number.' y];
