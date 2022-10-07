000956d3520bdd74409f4e9fe9ce515a24052755
Pedro Saman <pedrosaman96@gmail.com>
2022-10-07 13:59:37 +0900

    Update Aeq_beq_matrices_assembler.m
    Changed PN_i matrix to sparse matrix. This resolved the space and good part of memory allocation problems.

--- M_Files/Aeq_beq_matrices_assembler.m
+++ M_Files/Aeq_beq_matrices_assembler.m
@@ -8,34 +8,36 @@ function [Aeq,beq] = Aeq_beq_matrices_assembler(F,N,force,model,M)
 %       M: Block mass matrix
 %output: Aeq: A matrix 
 %        Beq: B matrix
-
     g = 9.8;
     Aeq = zeros(6*N, 3*force+1);
     beq = zeros(6*N, 1);
+    
     for n = 1 : N   % For each block in the model
         K_i = zeros(3, 3*force+1); % Upper portion of the "Wj" matrix from the formulation
-        p_i = zeros(1, 3); % All the "pk" vectors from the formulation into one matrix
-        PN_i = eye(3*force + 1); % The "A" matrix from the formulation. (A = diag(A1,A2,...,An))
+        PN_i = spalloc(3*force + 1,3*force + 1,3*force + 1); % The "A" matrix from the formulation. (A = diag(A1,A2,...,An))
         P_i = zeros(3, 3*force + 1); % Bottom portion of the "Wj" matrix from the formulation 
         % The Zero matrix inside A, case the kth force does not appear in the block is not in A but in Wj.
         for m = 1 : force 
             if(F(m, 2) == n) % If the current force first block is the current one
                 K_i(1:3, (3*m - 2):3*m) = eye(3); % Force Balance 
-                p_i(1:3) = F(m, 4:6); % Start point coordinates
+                p_i = F(m, 4:6); % The "pk" vectors from the formulation. Start point coordinates
                 P_i(1:3,(3*(m-1)+1):3*m) = [0, -p_i(3), p_i(2); p_i(3), 0, -p_i(1); -p_i(2), p_i(1), 0];
                 if(F(m, 7) == -1) % Correct the direction of force 
-                    PN_i((3*m - 2) : 3*m, (3*m - 2) : 3*m) = -eye(3);
+                    PN_i((3*m - 2) : 3*m, (3*m - 2) : 3*m) = sparse(-eye(3));
+                else
+                    PN_i((3*m - 2) : 3*m, (3*m - 2) : 3*m) = sparse(eye(3));
                 end
             elseif(F(m, 8) == n) % If the current force second block is the current one
                 K_i(1:3, (3*m - 2):3*m) = eye(3); % Force Balance
-                p_i(1:3) = F(m, 10:12); % Start point coordinates
+                p_i = F(m, 10:12); % The "pk" vectors from the formulation. Start point coordinates
                 P_i(1:3,(3*(m-1)+1):3*m) = [0, -p_i(3), p_i(2); p_i(3), 0, -p_i(1); -p_i(2), p_i(1), 0];
                 if(F(m, 13) == -1) % Correct the direction of force
-                    PN_i((3*m - 2) : 3*m, (3*m - 2) : 3*m) = -eye(3);
-                end
+                    PN_i((3*m - 2) : 3*m, (3*m - 2) : 3*m) = sparse(-eye(3));
+                else
+                    PN_i((3*m - 2) : 3*m, (3*m - 2) : 3*m) = sparse(eye(3));
+                end                
             end
         end
-        
         Aeq((6*n - 5) : 6*n, 1 : (3*force + 1)) = [K_i; P_i] * PN_i; %[W1*A1;W2*A2,...,Wn*An]
         %Mass changes depending on block type [b1;b2;...;bn]
         [col,row] = col_row_converter(model(n, 5));
