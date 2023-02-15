f6a7f18bd19f3db1fd04f475c0ab39d6c491bf54
pedro_taiga <pedrosaman96@gmail.com>
2023-02-15 14:01:55 +0900

    deleted files that dont need to be in git

--- ROS2_ws/build/denso_cobotta_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pedro/Mechanical_Analysis/ROS2_ws/src/denso_cobotta_description
-# Build directory: /home/pedro/Mechanical_Analysis/ROS2_ws/build/denso_cobotta_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ROS2_ws/build/denso_cobotta_moveit_config/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/pedro/Mechanical_Analysis/ROS2_ws/install/denso_cobotta_description/share/denso_cobotta_description/package.sh"
