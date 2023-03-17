d5502c56fbc851b4b44e7fdaa59dd14cafa092c4
pedro_taiga <pedrosaman96@gmail.com>
2023-03-17 17:47:35 +0900

    Created a interface to create the environment including block structure messages and services to spawn blocks into the scene

---
+++ ROS2_ws/src/environment_interface/src/block_spawner.cpp
@@ -0,0 +1,71 @@
+#include <rclcpp/rclcpp.hpp>
+#include <string>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <std_msgs/msg/color_rgba.hpp>
+#include "environment_interface/srv/block_service.hpp"
+#include "environment_interface/msg/block.hpp"
+#include <memory>
+
+#define BASE_NAME "base"
+#define block_size 0.3
+#define base_x_size 6.0
+#define base_y_size 6.0
+#define base_z_size 0.2
+#define dispenser_x_size 2.4
+#define dispenser_y_size 12.0
+#define dispenser_z_size 0.2
+#define minimum_resolution 0.01
+
+void add(const std::shared_ptr<environment_interface::srv::BlockService::Request> request,
+          std::shared_ptr<environment_interface::srv::BlockService::Response>       response)
+{
+  moveit_msgs::msg::CollisionObject block_object;
+  block_object.header.frame_id = request->block.frame_id;
+  block_object.id = request->block.name;
+  shape_msgs::msg::SolidPrimitive primitive;
+
+  // Define the size of the box in meters
+  primitive.type = primitive.BOX;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[primitive.BOX_X] = block_size*request->block.x_size - minimum_resolution;
+  primitive.dimensions[primitive.BOX_Y] = block_size*request->block.y_size - minimum_resolution;
+  primitive.dimensions[primitive.BOX_Z] = block_size - minimum_resolution;
+
+  // Define the pose of the box (relative to the frame_id)
+
+  geometry_msgs::msg::Pose block_pose;
+  block_pose.orientation.x = 0;
+  block_pose.orientation.y = 0;
+  block_pose.orientation.z = 0;
+  block_pose.position.x = -dispenser_x_size/2 + block_size*(request->block.x_size/2 + request->block.x);
+  block_pose.position.y = -dispenser_y_size/2 + block_size*(request->block.y_size/2 + request->block.y);
+  block_pose.position.z = dispenser_z_size/2 + block_size/2;
+
+  block_object.primitives.push_back(primitive);
+  block_object.primitive_poses.push_back(block_pose);
+  block_object.operation = block_object.ADD;
+
+  moveit::planning_interface::PlanningSceneInterface psi;
+  psi.applyCollisionObject(block_object, request->block.color);
+  
+  response->output = request->block.number;
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\nx: %.0f" " y: %.0f" " z: %.0f" " x_size: %.0f" " y_size: %.0f",  // CHANGE
+                request->block.x, request->block.y, request->block.z, request->block.x_size, request->block.y_size);                                         // CHANGE
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: created block number [%ld]", (long int)response->output);
+}
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("block_service_server");
+
+  rclcpp::Service<environment_interface::srv::BlockService>::SharedPtr service =
+    node->create_service<environment_interface::srv::BlockService>("block_service",  &add);
+
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to create a block.");
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+}
