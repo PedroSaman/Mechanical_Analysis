cb57e05801d4de203826e6865faa51f46cfce956
pedro_taiga <pedrosaman96@gmail.com>
2023-04-12 16:57:53 +0900

    lauch file to use terminal arguments to choose csv file PATH and name

--- ROS2_ws/src/manipulation_task_constructor/launch/pick_place_demo.launch.py
+++ ROS2_ws/src/manipulation_task_constructor/launch/pick_place_demo.launch.py
@@ -1,8 +1,26 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
 
 def generate_launch_description():
+
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "csv_file_path",
+            default_value="no_csv_provided",
+            description="Provides the assembly plan csv file path and name",
+        )
+    )
+    
+    return LaunchDescription(
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
+    )
+
+def launch_setup(context, *args, **kwargs):
+    test = LaunchConfiguration("csv_file_path")
     moveit_config = MoveItConfigsBuilder("denso_cobotta").to_dict()
 
     # MTC Demo node
@@ -12,7 +30,12 @@ def generate_launch_description():
         output="screen",
         parameters=[
             moveit_config,
+            {"csv_file_path": test},
         ],
     )
 
-    return LaunchDescription([pick_place_demo])
+    nodes_to_start = [
+        pick_place_demo,
+    ]
+
+    return nodes_to_start
