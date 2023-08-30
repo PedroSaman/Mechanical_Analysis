a8c5a733ab0209a7e1c7b9309b56927a71b3c19a
Pedro Saman <pedrosaman96@gmail.com>
2023-08-31 06:19:49 +0900

    Create Block_Model_Visualization_Formater.m
    converts block model format to assembly plan format. (only format)

---
+++ Matlab_Files/Assembly_Plan_Generator/Block_Model_Visualization_Formater.m
@@ -0,0 +1,12 @@
+function Block_Model_Visualization_Formater(filename)
+%Creates a file in the same formatation of a assembly plan but without
+%making any changes to the block model in the input. This is interesting to
+%visualize the block model using the ROS2 simulator.
+
+directory = "../../Block_Printer_System/Models/BlockModel/";
+block_model = model_loader(directory + filename + ".txt"); % Load model and adapt it to the Stability Judge format
+plan_formatation(block_model,directory + filename + "_visualization" + ".txt");
+fprintf("\nModel completed. Plan available in %s\n",directory + filename + ".txt");
+
+end
+
