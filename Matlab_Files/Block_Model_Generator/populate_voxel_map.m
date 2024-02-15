cb165088749fb9e7107dad2f550321ad0c68c837
Pedro Saman <pedrosaman96@gmail.com>
2024-02-16 06:25:32 +0900

    Update populate_voxel_map.m
    added a line to use only the small block types if needed.

--- Matlab_Files/Block_Model_Generator/populate_voxel_map.m
+++ Matlab_Files/Block_Model_Generator/populate_voxel_map.m
@@ -7,8 +7,10 @@ function [block_to_insert, layer_map] = populate_voxel_map(layer_map,voxel_index
     priority_block_list = mod(voxel_index(3) - 1,2); % Odd and even layers have different block list priority
     if(priority_block_list) % Set the priority list for block placement
         block_list = [[8,2];[2,8];[4,2];[2,4];[3,2];[2,3];[4,1];[1,4];[2,2];[3,1];[1,3];[2,1];[1,2];[1,1]];
+        %block_list = [[2,2];[2,1];[1,2];[1,1]];
     else
        block_list = [[2,8];[8,2];[2,4];[4,2];[2,3];[3,2];[1,4];[4,1];[2,2];[1,3];[3,1];[1,2];[2,1];[1,1]]; 
+       %block_list = [[2,2];[1,2];[2,1];[1,1]]; 
     end
 
     block_list_size = size(block_list,1);
@@ -25,4 +27,3 @@ function [block_to_insert, layer_map] = populate_voxel_map(layer_map,voxel_index
     end
     block_to_insert = -1; %should never return from here, at least the 1x1 should fit
 end
-
